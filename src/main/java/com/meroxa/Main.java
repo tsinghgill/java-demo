94f732714d85dd0f8d5e582fb11aa6231dd3eee2
Meroxa <tanveet.gill@gmail.com>
2023-06-12 10:03:01 -0400

    test

--- src/main/java/com/meroxa/Main.java
+++ src/main/java/com/meroxa/Main.java
@@ -1,15 +1,11 @@
 package com.meroxa;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.meroxa.turbine.Turbine;
 import com.meroxa.turbine.TurbineApp;
 import com.meroxa.turbine.TurbineRecord;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
 
 @ApplicationScoped
 public class Main implements TurbineApp {
@@ -24,29 +20,15 @@ public class Main implements TurbineApp {
     }
 
     private List<TurbineRecord> process(List<TurbineRecord> records) {
-        List<TurbineRecord> processedStream = new ArrayList<>();
-        ObjectMapper mapper = new ObjectMapper();
-
-        for (TurbineRecord record : records) {
-            String payload = record.getPayload();
-
-            try {
-                Map<String, Object> payloadMap = mapper.readValue(payload, new TypeReference<Map<String, Object>>() {});
-                Map<String, String> sourceMap = (Map<String, String>)payloadMap.get("source");
-                if (sourceMap != null) {
-                    String source = sourceMap.get("name");
-                    if ("west-store-mongo".equals(source)) {
-                        payloadMap.put("storeId", "001");
-                        String updatedPayload = mapper.writeValueAsString(payloadMap);
-                        record.setPayload(updatedPayload);
-                        processedStream.add(record);
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("Error while processing record: " + e.getMessage());
-            }
-        }
-
-        return processedStream;
+        return records.stream()
+            .filter(turbineRecord -> {
+                String source = (String) turbineRecord.jsonGet("$.payload.after.source");
+                return "west-store-mongo".equals(source);
+            })
+            .map(record -> {
+                record.jsonSet("$.payload.after.source", "store-id-001");
+                return record;
+            })
+            .toList();
     }
 }
